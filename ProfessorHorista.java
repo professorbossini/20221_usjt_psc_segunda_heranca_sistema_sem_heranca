da954e288c291960492615031f18c2d243e8e3bc
Rodrigo Bossini <professorbossini@gmail.com>
2022-04-25 19:41:28 -0300

    ProfessorHorista e ProfessorPesquisador, sem heranca

---
+++ ProfessorHorista.java
@@ -0,0 +1,5 @@
+public class ProfessorHorista {
+    private String nome;
+    private int idade;
+    //getters/setters
+}
---
+++ ProfessorPesquisador.java
@@ -0,0 +1,8 @@
+public class ProfessorPesquisador {
+    private String nome;
+    private int idade;
+    //getters/setters
+    public void pesquisar(){
+        System.out.println(nome + " pesquisando...");
+    }
+}
